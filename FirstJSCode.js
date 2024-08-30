b4ee45fe0c05c6c236f8bbd9f4ae647822fe90fa
CoderTahmid <tahmid.ibne@gmail.com>
2024-08-30 06:40:01 +0600

    complex condition writting structure added

---
+++ FirstJSCode.js
@@ -0,0 +1,12 @@
+let salary = 25000;
+let hasCar = true;
+let isBCS = true;
+
+if((salary > 25000 && hasCar == true) || isBCS == true) {
+        console.log("shu patro");
+}
+
+if((salary > 25000 || hasCar == true) && isBCS == true) {
+    console.log("shu patro")
+}
+    
