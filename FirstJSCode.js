5d636e5c49d724e763a5b388ab1216aab44c6938
CoderTahmid <tahmid.ibne@gmail.com>
2024-08-31 09:12:39 +0600

    includes method added in array

--- FirstJSCode.js
+++ FirstJSCode.js
@@ -1,12 +1,2 @@
-let salary = 25000;
-let hasCar = true;
-let isBCS = true;
-
-if((salary > 25000 && hasCar == true) || isBCS == true) {
-        console.log("shu patro");
-}
-
-if((salary > 25000 || hasCar == true) && isBCS == true) {
-    console.log("shu patro")
-}
-    
+let friends = ["rifat", "maruf", "siam", "ayon"];
+console.log(friends.includes("Rifat"));
