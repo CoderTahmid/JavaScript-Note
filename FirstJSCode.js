415bbefceb17f8ef878befca5189c01e5e246da8
CoderTahmid <tahmid.ibne@gmail.com>
2024-09-03 19:32:32 +0600

    object value and key related 1 important point added

--- FirstJSCode.js
+++ FirstJSCode.js
@@ -1,13 +1,8 @@
-/* Looping over an object */
-
-const mobile = {
-    brand: 'samsung',
-    price: '25000',
-    color: 'black',
-    camera: '12mp',
-    isNew: true
-}
-for(const prop in mobile){
-    console.log(prop); // mobile object'r all key gula ke output hishabe dekhabe
-    console.log(mobile[prop]); // all key'r value gula ke output hishabe dekhabe
-}
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const key = Object.values(student);
+console.log(key.length);
