82c36d381ee58c9f59913c6f3968f7284a5da1e3
CoderTahmid <tahmid.ibne@gmail.com>
2024-09-03 07:49:11 +0600

    join method added

---
+++ FirstJSCode.js
@@ -0,0 +1,6 @@
+/*Join method*/
+/*eta mulot ekta arrya'r element gula ke join korayh*/
+const friends = ['rahim', 'kahim', 'fahim', 'mahim', 'sahim'];
+console.log(friends.join()); // output : rahim,kahim,fahim,mahim,sahim
+console.log(friends.join('|')); // output : rahim|kahim|fahim|mahim|sahim
+console.log(friends.join('-')); // output : rahim-kahim-fahim-mahim-sahim
