2b8663a24778f0a2dd940a4a48a83265b68eb59c
CoderTahmid <tahmid.ibne@gmail.com>
2024-09-08 21:57:42 +0600

    something about API fetching added

--- FirstJSCode.js
+++ FirstJSCode.js
@@ -1,12 +1,13 @@
-function loadData() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
-
-function loadUsers() {
+function loadUsers2() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(data => console.log(data));
-}
-
+    .then(data => displayUsers2(data));
+};
+function displayUsers2(data) {
+    const ul = document.getElementById('users-list');
+    for(const user of data) {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    };
+};
