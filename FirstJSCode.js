9e6a2877619291a92957cffef36cd0781fac75f4
CoderTahmid <tahmid.ibne@gmail.com>
2024-09-08 21:08:27 +0600

    stringify method of API added

--- FirstJSCode.js
+++
@@ -1,9 +0,0 @@
-let numArr4 = [1, 2, 3, 4, 5, 6];
-numArr4.map((val) => {
-    console.log(val); /*this will result as like for-each loop*/
-});
-
-new_saved_array = numArr4.map((val) => {
-    return val;
-})
-console.log(new_saved_array);
